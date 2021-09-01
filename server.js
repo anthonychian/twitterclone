512ebce49920610a46720fdeba7788ec5c9c6e43
Anthony C <achian@ucsc.edu>
2021-08-31 22:59:58 -0700

    add routes to server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//using express for API
 const express = require('express');
-const path = require('path');
+require('dotenv').config()
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use('/post/', require('./routes/api/post'));
+app.use('/auth/', require('./routes/api/auth'));
+app.use('/user/', require('./routes/api/user'));
+
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
